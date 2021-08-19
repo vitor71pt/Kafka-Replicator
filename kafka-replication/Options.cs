aba9a95591c52e44eba3e16a668b978e6e77a936
Vitor Rodrigues <vitor71rodrigues@gmail.com>
2021-08-19 18:30:42 +0100

    Add executable logic

---
+++ kafka-replication/Options.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine;
+
+namespace kafka_replication
+{
+
+    public class Options
+    {
+        [Value(0, Required = true, HelpText = "Source Bootstraper Server")]
+        public string SourceServer { get; set; }
+
+        [Value(1, Required = true, HelpText = "Target Bootstraper Server.")]
+        public string TargetServer { get; set; }
+
+        [Value(2, Required = true, HelpText = "TopicName.")]
+        public string Topic { get; set; }
+    }
+}
+
